85f86b846d412c84cadece053e655a2286c17d3a
Bernard Gresola <68579975+bernard-uerm@users.noreply.github.com>
2021-09-15 21:40:13 +0800

    Initial Commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div  class="bg-image">
+    <q-layout view="lHh Lpr lFf">
+      <q-page-container>
+        <router-view />
+      </q-page-container>
+    </q-layout>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+
+  .bg-image {
+    background-image: url(https://uerm.edu.ph/assets/3.0/js/style-switcher/images/bg_red_2012.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
+
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-card style="width:800px;max-width:900px;">
+      <q-card-section align="center" class="text-h4">
+        UERM ACADEME
+      </q-card-section>
+      <q-card-section>
+        <div class="row q-col-gutter-md">
+          <div class="col-5">
+            <q-card>
+              <q-card-section>
+                SECTION HERE
+              </q-card-section>
+            </q-card>
+          </div>
+          <div class="col-7">
+            <q-card>
+              <q-card-section>
+                SECTION HERE
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
